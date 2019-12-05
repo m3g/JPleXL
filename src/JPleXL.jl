38fb23f7e66eef505ba1d85c78145c84f9995e4e
Leandro Martinez <leandro@iqm.unicamp.br>
2019-12-05 12:42:30 -0300

    removed the PDBTools files

--- src/JPleXL.jl
+++ src/JPleXL.jl
@@ -1,29 +1,6 @@
 
 module JPleXL
 
-  using Printf
-
-  #
-  # Data structures
-  #
-
-  include("./empty_struct.jl")
-
-  include("./Atom.jl")
-  export Atom
-
-  include("./Indexes_mmCIF_fields.jl")
-
-  #
-  # Input and output functions
-  #
-
-  include("./check_mmCIF.jl")
-  include("./read_atom.jl")
-  include("./write_atom.jl")
-  include("./readPDB.jl")
-  export readPDB
-
   # Data for performance code
  
   include("./xCA.jl")
